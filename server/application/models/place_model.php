970704cf0f1a2115d4dd7e5dc151876eebc7afb4
spondbob <sponbob_squelpen@yahoo.com>
2013-03-26 17:45:30 +0700

    query nearby place using Haversine Formula

--- server/application/models/place_model.php
+++ server/application/models/place_model.php
@@ -1,6 +1,7 @@
 <?php
 class Place_model extends CI_Model
 {
+	private $table = "place";
 	public $place_id;
 	public $place_name;
 	public $place_desc;
@@ -11,6 +12,25 @@ class Place_model extends CI_Model
 	function __construct()
 	{
 		$this->load->database();
+	}
+
+	public function list_in_range($distance = 1000)
+	{
+		return $this->db->query("
+			SELECT place_name,
+				(
+					6371000 *
+					acos(
+						cos( radians(" . $this->place_lat . ") ) *
+						cos( radians( place_lat ) ) *
+						cos( radians(place_lng) - radians(" . $this->place_lng . ") ) +
+						sin( radians(" . $this->place_lat . ") ) *
+						sin( radians(place_lat) )
+					)
+				) AS distance
+			FROM " . $this->table . "
+			HAVING distance < $distance
+			ORDER BY distance");
 	}
 }
 ?>
