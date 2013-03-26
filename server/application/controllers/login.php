cbf9eab76aa6f1fd88c49c1573e28bf860a5fe5e
spondbob <sponbob_squelpen@yahoo.com>
2013-03-26 18:26:13 +0700

    login_controller -> login

---
+++ server/application/controllers/login.php
@@ -0,0 +1,22 @@
+<?php
+class Login extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array('user_model'));
+	}
+	
+	public function index()
+	{
+		if($this->input->post('userName') && $this->input->post('userPassword'))
+		{
+			$model = new User_model;
+			$model->userName = $this->input->post('userName');
+			$model->userPassword = md5($this->input->post('userPassword'));
+
+			echo $model->login();
+		}
+	}
+}
+?>
