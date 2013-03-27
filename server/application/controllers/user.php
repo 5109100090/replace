a643d952fd261c069daffbe3101d1c4bc6e7cbea
spondbob <sponbob_squelpen@yahoo.com>
2013-03-27 20:43:57 +0700

    merge from 'login'

---
+++ server/application/controllers/user.php
@@ -0,0 +1,22 @@
+<?php
+class User extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array('user_model'));
+	}
+	
+	public function login()
+	{
+		if($this->input->post('userName') && $this->input->post('userPassword'))
+		{
+			$model = new User_model;
+			$model->userName = $this->input->post('userName');
+			$model->userPassword = md5($this->input->post('userPassword'));
+
+			echo '['.json_encode($model->login()).']';
+		}
+	}
+}
+?>
