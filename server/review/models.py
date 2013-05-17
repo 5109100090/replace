f8c4b517e2389a09b520769c6dff85e29d013eaf
spondbob <sponbob_squelpen@yahoo.com>
2013-05-17 10:37:07 +0700

    review query w/ similarity

--- server/review/models.py
+++ server/review/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from authenticate.models import User
 from place.models import Place
 
+class ReviewManager(models.Manager):
+    def listReviews(self, user, place):
+        query = "SELECT *, (reviewPoint*similarityValue) AS newSimilarityValue \
+            FROM review_review r \
+            RIGHT JOIN similarity_similarity s \
+            ON (r.reviewUser_id = s.similarityUser1_id AND s.similarityUser2_id = %s) OR \
+                (r.reviewUser_id = s.similarityUser2_id AND s.similarityUser1_id = %s) \
+            WHERE reviewPlace_id = %s \
+            ORDER BY newSimilarityValue DESC"
+        
+        return Review.objects.raw(query, [user.userId, user.userId, place.placeId])
+
 class Review(models.Model):
     reviewId = models.AutoField(primary_key=True)
     reviewUser = models.ForeignKey(User)
     reviewPlace = models.ForeignKey(Place)
     reviewPoint = models.DecimalField(decimal_places=0, max_digits=10)
+    objects = ReviewManager()
--- server/review/views.py
+++ server/review/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from review.models import Review
+from authenticate.models import User
 from place.models import Place
 import json
 
@@ -8,14 +9,19 @@ def index(request):
     
 def listReviews(request):
     if request.method == "POST" :
+        user = User()
+        user.userId = int(request.POST["userId"])
+        
         place = Place()
         place.placeId = int(request.POST["placeId"])
         
         data = []       
-        for r in Review.objects.filter(reviewPlace_id=place) :
+        for r in Review.objects.listReviews(user, place) :
             dict = {}
             dict['userAlias'] = r.reviewUser.userAlias
             dict['reviewPoint'] = str(r.reviewPoint)
+            dict['similarityValue'] = str(r.similarityValue)
+            dict['newSimilarityValue'] = str(r.newSimilarityValue)
             data.append(dict)
         return HttpResponse(json.dumps(data))
     else :
