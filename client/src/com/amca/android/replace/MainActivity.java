143c96d8b732dc9dd3ee4d75741c15cbd3861df4
spondbob <sponbob_squelpen@yahoo.com>
2013-05-25 11:49:47 +0700

    error handling on login

--- client/src/com/amca/android/replace/MainActivity.java
+++ client/src/com/amca/android/replace/MainActivity.java
@@ -2,10 +2,7 @@ package com.amca.android.replace;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,9 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.amca.android.replace.http.HTTPTransfer;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,21 +22,24 @@ public class MainActivity extends Activity {
 
 	private TextView registerScreen;
 	private EditText userName, userPassword;
-	private Button buttonLogin, quickButtonLogin;
+	private Button buttonLogin;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setTitle("Login to your Account");
-		//this.prepareConfiguration();
-		
+
 		userName = (EditText) findViewById(R.id.userName);
 		userPassword = (EditText) findViewById(R.id.userPassword);
 		buttonLogin = (Button) findViewById(R.id.buttonLogin);
 		buttonLogin.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-            	doLogin(userName.getText().toString(), userPassword.getText().toString());
+                if(userName.getText().toString().equals("") || userPassword.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "input your username and password", Toast.LENGTH_SHORT).show();
+                }else{
+                    doLogin(userName.getText().toString(), userPassword.getText().toString());
+                }
             }
         });
         registerScreen = (TextView) findViewById(R.id.linkToRegister);
@@ -52,14 +50,6 @@ public class MainActivity extends Activity {
 		    	startActivity(intent);
 			}
 		});
-		/*/
-		quickButtonLogin = (Button) findViewById(R.id.quickButtonLogin);
-		quickButtonLogin.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-            	doLogin("rizky", "123qwe");
-            }
-        });
-        //*/
 	}
 	
 	@Override
@@ -85,27 +75,20 @@ public class MainActivity extends Activity {
 		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("userName", username);
 		data.put("userPassword", password);
+
 		HTTPLogin login = new HTTPLogin();
 		login.setCtx(MainActivity.this);
 		login.setData(data);
 		login.execute("authenticate/login/");
 	}
-	
-	private void prepareConfiguration(){
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-		Editor editor = preferences.edit();
-		editor.putString("serverUrl", "http://10.151.36.36:8080/");
-		editor.putString("geolocation", "static");
-		editor.commit();
-	}
-	
+
 	class HTTPLogin extends HTTPTransfer{
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
 			if(result.equals("null")){
 				System.out.println("invalid username & password");
-	        	Toast.makeText(getApplicationContext(), "invalid username & password", Toast.LENGTH_SHORT).show();
+	        	Toast.makeText(this.getCtx(), "invalid username & password", Toast.LENGTH_SHORT).show();
 			}else{
 				try{
 					JSONArray jArray = new JSONArray(result);
@@ -116,7 +99,7 @@ public class MainActivity extends Activity {
 		        	intent.putExtra("userAlias", json_data.getString("userAlias"));
 		        	startActivity(intent);
 				}catch(JSONException e){
-					Toast.makeText(getApplicationContext(), "Error parsing data "+e.toString(), Toast.LENGTH_SHORT).show();
+					Toast.makeText(this.getCtx(), "Error parsing data "+e.toString(), Toast.LENGTH_SHORT).show();
 				}
 			}
 		}
--- server/authenticate/views.py
+++ server/authenticate/views.py
@@ -9,9 +9,13 @@ def login(request):
     if request.method == "POST" :
         userName = str(request.POST["userName"])
         userPassword = hashlib.md5(str(request.POST["userPassword"])).hexdigest()
-        u = User.objects.get(userName=userName,userPassword=userPassword)
-        data = [ { 'userId':u.userId, 'userAlias':u.userAlias } ]
-        return HttpResponse(json.dumps(data))
+
+        if User.objects.filter(userName=userName,userPassword=userPassword).exists():
+            u = User.objects.get(userName=userName,userPassword=userPassword)
+            data = json.dumps( [ { 'userId':u.userId, 'userAlias':u.userAlias } ] )
+        else:
+            data = "null"
+        return HttpResponse(data)
     else :
         return HttpResponse("what?")
     
