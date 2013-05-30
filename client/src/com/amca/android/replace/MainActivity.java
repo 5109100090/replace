29b8d7c3ba26c4294d2471fa091a5a2115e51b82
spondbob <sponbob_squelpen@yahoo.com>
2013-05-30 11:03:08 +0700

    tidy up & moving to new package

--- client/src/com/amca/android/replace/MainActivity.java
+++ client/src/com/amca/android/replace/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.amca.android.replace.http.HTTPTransfer;
+import com.amca.android.replace.user.UserForm;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +27,7 @@ public class MainActivity extends Activity {
 	private TextView registerScreen;
 	private EditText userName, userPassword;
 	private Button buttonLogin;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,24 +38,28 @@ public class MainActivity extends Activity {
 		userPassword = (EditText) findViewById(R.id.userPassword);
 		buttonLogin = (Button) findViewById(R.id.buttonLogin);
 		buttonLogin.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if(userName.getText().toString().equals("") || userPassword.getText().toString().equals("")){
-                    Toast.makeText(MainActivity.this, "input your username and password", Toast.LENGTH_SHORT).show();
-                }else{
-                    doLogin(userName.getText().toString(), userPassword.getText().toString());
-                }
-            }
-        });
-        registerScreen = (TextView) findViewById(R.id.linkToRegister);
-        registerScreen.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-            	Intent intent = new Intent(MainActivity.this, UserForm.class);
-            	intent.putExtra("mode", "register");
-		    	startActivity(intent);
+				if (userName.getText().toString().equals("")
+						|| userPassword.getText().toString().equals("")) {
+					Toast.makeText(MainActivity.this,
+							"input your username and password",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					doLogin(userName.getText().toString(), userPassword
+							.getText().toString());
+				}
+			}
+		});
+		registerScreen = (TextView) findViewById(R.id.linkToRegister);
+		registerScreen.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, UserForm.class);
+				intent.putExtra("mode", "register");
+				startActivity(intent);
 			}
 		});
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -62,20 +68,20 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item){
-    	switch (item.getItemId()) {
-			case R.id.action_settings:
-				Intent intent = new Intent(MainActivity.this, Settings.class);
-		    	startActivity(intent);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Intent intent = new Intent(MainActivity.this, Settings.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-    }
-	
-	private void doLogin(String username, String password){
-		progressDialog = ProgressDialog.show(MainActivity.this,"Loading...",  
-        	    "Authenticate your data", false, false); 
+	}
+
+	private void doLogin(String username, String password) {
+		progressDialog = ProgressDialog.show(MainActivity.this, "Loading...",
+				"Authenticate your data", false, false);
 		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("userName", username);
 		data.put("userPassword", password);
@@ -86,28 +92,33 @@ public class MainActivity extends Activity {
 		login.execute("authenticate/login/");
 	}
 
-	class HTTPLogin extends HTTPTransfer{
+	class HTTPLogin extends HTTPTransfer {
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
-			if(result.equals("null")){
+			if (result.equals("null")) {
 				System.out.println("invalid username & password");
-	        	Toast.makeText(this.getContext(), "invalid username & password", Toast.LENGTH_SHORT).show();
-			}else{
-				try{
+				Toast.makeText(this.getContext(),
+						"invalid username & password", Toast.LENGTH_SHORT)
+						.show();
+			} else {
+				try {
 					JSONArray jArray = new JSONArray(result);
 					JSONObject json_data = jArray.getJSONObject(0);
-					
-					Intent intent = new Intent(MainActivity.this, PlaceType.class);
-		        	intent.putExtra("userId", json_data.getInt("userId"));
-		        	intent.putExtra("userAlias", json_data.getString("userAlias"));
-		        	startActivity(intent);
-				}catch(JSONException e){
-					Toast.makeText(this.getContext(), "Error parsing data "+e.toString(), Toast.LENGTH_SHORT).show();
+
+					Intent intent = new Intent(MainActivity.this,
+							PlaceType.class);
+					intent.putExtra("userId", json_data.getInt("userId"));
+					intent.putExtra("userAlias",
+							json_data.getString("userAlias"));
+					startActivity(intent);
+				} catch (JSONException e) {
+					Toast.makeText(this.getContext(),
+							"Error parsing data " + e.toString(),
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 			progressDialog.dismiss();
 		}
 	}
 }
-
--- client/src/com/amca/android/replace/PlaceType.java
+++ client/src/com/amca/android/replace/PlaceType.java
@@ -19,6 +19,8 @@ import android.widget.Toast;
 import com.amca.android.replace.gps.GPSTracker;
 import com.amca.android.replace.http.HTTPTransfer;
 import com.amca.android.replace.model.Type;
+import com.amca.android.replace.place.PlaceSelector;
+import com.amca.android.replace.user.UserForm;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,25 +30,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlaceType extends Activity implements OnClickListener {
-	
+
 	private Integer userId;
 	private String currentLat, currentLng;
-    private boolean autoLocation;
+	private boolean autoLocation;
 	private List<Type> typeList = new ArrayList<Type>();
 	private TextView textViewPlaceType, textViewRange;
 	private Spinner spinnerPlaceType, spinnerRange;
 	private Button buttonSubmit;
 	private ProgressBar progressBar;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_place_type);
-		
+
 		Intent intent = getIntent();
 		this.userId = intent.getIntExtra("userId", 0);
 		setTitle("Welcome " + intent.getStringExtra("userAlias"));
-		
+
 		textViewPlaceType = (TextView) findViewById(R.id.textViewPlaceType);
 		textViewRange = (TextView) findViewById(R.id.textViewRange);
 		spinnerPlaceType = (Spinner) findViewById(R.id.spinnerPlaceType);
@@ -54,90 +56,101 @@ public class PlaceType extends Activity implements OnClickListener {
 		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
 		buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
 		buttonSubmit.setOnClickListener(this);
-		
+
 		progressBar.setVisibility(View.VISIBLE);
 		textViewPlaceType.setVisibility(View.INVISIBLE);
 		textViewRange.setVisibility(View.INVISIBLE);
 		spinnerPlaceType.setVisibility(View.INVISIBLE);
 		spinnerRange.setVisibility(View.INVISIBLE);
 		buttonSubmit.setVisibility(View.INVISIBLE);
-		
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(PlaceType.this);
-        autoLocation = preferences.getBoolean("autoLocation", false);
-		
-		if(autoLocation){
-            GPSTracker gps = new GPSTracker(PlaceType.this);
-            if(gps.canGetLocation()){
-                Double latitude = gps.getLatitude();
-                Double longitude = gps.getLongitude();
-                this.currentLat = latitude.toString();
-                this.currentLng = longitude.toString();
-                Toast.makeText(getApplicationContext(), "Your Location is \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-            }else{
-                gps.showSettingsAlert();
-            }
-		}else{
-            this.currentLat = "-7.27957";
-            this.currentLng = "112.79751";
+
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(PlaceType.this);
+		autoLocation = preferences.getBoolean("autoLocation", false);
+
+		if (autoLocation) {
+			GPSTracker gps = new GPSTracker(PlaceType.this);
+			if (gps.canGetLocation()) {
+				Double latitude = gps.getLatitude();
+				Double longitude = gps.getLongitude();
+				this.currentLat = latitude.toString();
+				this.currentLng = longitude.toString();
+				if (autoLocation) {
+					progressBar.setVisibility(View.INVISIBLE);
+					textViewPlaceType.setVisibility(View.VISIBLE);
+					textViewRange.setVisibility(View.VISIBLE);
+					spinnerPlaceType.setVisibility(View.VISIBLE);
+					spinnerRange.setVisibility(View.VISIBLE);
+					buttonSubmit.setVisibility(View.VISIBLE);
+				}
+			} else {
+				gps.showSettingsAlert();
+			}
+		} else {
+			this.currentLat = "-7.27957";
+			this.currentLng = "112.79751";
 		}
-		
+
 		HTTPPlaceType http = new HTTPPlaceType();
 		http.setContext(PlaceType.this);
 		http.setMode(2);
 		http.execute("type/listAll/");
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		int position = spinnerPlaceType.getSelectedItemPosition();
 		Type type = typeList.get(position);
-		if(type.getTypeTotal() > 0){
+		if (type.getTypeTotal() > 0) {
 			Intent intent = new Intent(PlaceType.this, PlaceSelector.class);
-			String [] range = String.valueOf(spinnerRange.getSelectedItem()).split(" meter");
-        	intent.putExtra("range", range[0]);
-        	intent.putExtra("userId", this.userId);
-        	intent.putExtra("typeId", type.getTypeId());
-        	intent.putExtra("typeName", type.getTypeName());
-        	intent.putExtra("currentLat", this.currentLat);
-        	intent.putExtra("currentLng", this.currentLng);
-        	startActivity(intent);
-		}else{
-			Toast.makeText(PlaceType.this, "no data available for " + type.getTypeName(), Toast.LENGTH_SHORT).show();
+			String[] range = String.valueOf(spinnerRange.getSelectedItem())
+					.split(" meter");
+			intent.putExtra("range", range[0]);
+			intent.putExtra("userId", this.userId);
+			intent.putExtra("typeId", type.getTypeId());
+			intent.putExtra("typeName", type.getTypeName());
+			intent.putExtra("currentLat", this.currentLat);
+			intent.putExtra("currentLng", this.currentLng);
+			startActivity(intent);
+		} else {
+			Toast.makeText(PlaceType.this,
+					"no data available for " + type.getTypeName(),
+					Toast.LENGTH_SHORT).show();
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.place_type, menu);
 		return true;
 	}
-	
+
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item){
-    	switch (item.getItemId()) {
-			case R.id.action_profile:
-				Intent intent = new Intent(PlaceType.this, UserForm.class);
-				intent.putExtra("mode", "update");
-				intent.putExtra("userId", this.userId.toString());
-		    	startActivity(intent);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_profile:
+			Intent intent = new Intent(PlaceType.this, UserForm.class);
+			intent.putExtra("mode", "update");
+			intent.putExtra("userId", this.userId.toString());
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-    }
+	}
 	
-	class HTTPPlaceType extends HTTPTransfer{
+	class HTTPPlaceType extends HTTPTransfer {
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
-			//parse json data
-			try{
+			// parse json data
+			try {
 				List<String> list = new ArrayList<String>();
 				JSONArray jArray = new JSONArray(result);
-				for(int i=0;i<jArray.length();i++){
+				for (int i = 0; i < jArray.length(); i++) {
 					JSONObject json_data = jArray.getJSONObject(i);
-					
+
 					Type type = new Type();
 					type.setTypeId(json_data.getInt("typeId"));
 					type.setTypeName(json_data.getString("typeName"));
@@ -145,19 +158,26 @@ public class PlaceType extends Activity implements OnClickListener {
 					list.add(type.getTypeName());
 					typeList.add(type);
 				}
-				ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, list);
-				dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
+						getApplicationContext(),
+						android.R.layout.simple_spinner_item, list);
+				dataAdapter
+						.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 				spinnerPlaceType.setAdapter(dataAdapter);
-			}catch(JSONException e){
-				Toast.makeText(getApplicationContext(), "Error parsing data "+e.toString(), Toast.LENGTH_SHORT).show();
+			} catch (JSONException e) {
+				Toast.makeText(getApplicationContext(),
+						"Error parsing data " + e.toString(),
+						Toast.LENGTH_SHORT).show();
 			}
-			
-			progressBar.setVisibility(View.INVISIBLE);
-			textViewPlaceType.setVisibility(View.VISIBLE);
-			textViewRange.setVisibility(View.VISIBLE);
-			spinnerPlaceType.setVisibility(View.VISIBLE);
-			spinnerRange.setVisibility(View.VISIBLE);
-			buttonSubmit.setVisibility(View.VISIBLE);
+
+			if (!autoLocation) {
+				progressBar.setVisibility(View.INVISIBLE);
+				textViewPlaceType.setVisibility(View.VISIBLE);
+				textViewRange.setVisibility(View.VISIBLE);
+				spinnerPlaceType.setVisibility(View.VISIBLE);
+				spinnerRange.setVisibility(View.VISIBLE);
+				buttonSubmit.setVisibility(View.VISIBLE);
+			}
 		}
-	}  
+	}
 }
