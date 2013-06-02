0984afd41ccbd0e4278f797922038b02b040522e
spondbob <sponbob_squelpen@yahoo.com>
2013-06-02 21:22:06 +0700

    implement image loader

--- client/src/com/amca/android/replace/place/PlaceSelectorArrayAdapter.java
+++ client/src/com/amca/android/replace/place/PlaceSelectorArrayAdapter.java
@@ -4,11 +4,16 @@ import java.text.DecimalFormat;
 import java.util.List;
 import com.amca.android.replace.R;
 import com.amca.android.replace.model.Place;
+import com.fedorvlasov.lazylist.ImageLoader;
+
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlaceSelectorArrayAdapter extends
@@ -36,7 +41,8 @@ public class PlaceSelectorArrayAdapter extends
 				.findViewById(R.id.placePoint);
 		TextView placeDistance = (TextView) rowView
 				.findViewById(R.id.placeDistance);
-
+		ImageView placeIcon = (ImageView) rowView.findViewById(R.id.placeIcon);
+		
 		DecimalFormat df = new DecimalFormat("#0.##");
 
 		Place place = values.get(position);
@@ -45,6 +51,11 @@ public class PlaceSelectorArrayAdapter extends
 		placeDesc.setText(place.getPlaceDesc());
 		placePoint.setText(df.format(place.getAveragePoint()));
 		placeDistance.setText(place.getPlaceReviews() + " review(s) | " + df.format(place.getPlaceDistance() / 1000) + " km");
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+		String serverUrl = preferences.getString("serverUrl", "");
+		ImageLoader imageLoader = new ImageLoader(context);
+		imageLoader.DisplayImage(serverUrl + "static/place/" + place.getPlaceId() + ".jpg", placeIcon);
 
 		return rowView;
 	}
