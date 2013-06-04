7876cfc23a4dc284417326c81a74f981135c4033
spondbob <sponbob_squelpen@yahoo.com>
2013-06-04 21:47:31 +0700

    new layout

--- client/src/com/amca/android/replace/place/PlaceDetail.java
+++ client/src/com/amca/android/replace/place/PlaceDetail.java
@@ -1,30 +1,52 @@
 package com.amca.android.replace.place;
 
+import java.text.DecimalFormat;
+
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
-import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.app.SherlockActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.amca.android.replace.R;
 import com.amca.android.replace.review.PlaceReviews;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
-public class PlaceDetail extends SherlockListActivity {
+public class PlaceDetail extends SherlockActivity {
 
 	private Integer userId, placeId;
-	private String placeName;
+	private String placeNameValue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_place_detail);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
+
 		Intent intent = getIntent();
 		this.userId = intent.getIntExtra("userId", 0);
 		this.placeId = intent.getIntExtra("placeId", 0);
-		this.placeName = intent.getStringExtra("placeName");
-		setTitle(this.placeName);
+		this.placeNameValue = intent.getStringExtra("placeName");
+		setTitle(this.placeNameValue);
+		
+		DecimalFormat df = new DecimalFormat("#0.##");
+		((TextView) findViewById(R.id.placeName)).setText(intent.getStringExtra("placeName"));
+		((TextView) findViewById(R.id.placeAddress)).setText(intent.getStringExtra("placeAddress"));
+		((TextView) findViewById(R.id.placeDesc)).setText(intent.getStringExtra("placeDesc"));
+		((TextView) findViewById(R.id.placeReviews)).setText(intent.getStringExtra("placeReviews") + " review(s) | " + df.format(intent.getDoubleExtra("placeDistance", 0)/1000) + " km");
+		((TextView) findViewById(R.id.placePoint)).setText(df.format(intent.getDoubleExtra("averagePoint", 0)));
+
+		ImageView placeIcon = (ImageView) findViewById(R.id.placeIcon);
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		String serverUrl = preferences.getString("serverUrl", "");
+		
+		UrlImageViewHelper.setUrlDrawable(placeIcon, serverUrl + "static/place/" + this.placeId + ".jpg", R.drawable.gif_loader);
 	}
 
 	@Override
@@ -39,12 +61,12 @@ public class PlaceDetail extends SherlockListActivity {
 				Intent intent = new Intent(PlaceDetail.this, PlaceReviews.class);
 				intent.putExtra("userId", userId);
 				intent.putExtra("placeId", placeId);
-				intent.putExtra("placeName", placeName);
+				intent.putExtra("placeName", placeNameValue);
 				startActivity(intent);
 				return true;
 			}
 		});
-		
+
 		return true;
 	}
 }
