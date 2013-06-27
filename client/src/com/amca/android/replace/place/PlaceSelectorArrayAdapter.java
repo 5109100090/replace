2f2269b7ee9792e1727c523840b17cf254cdc221
spondbob <sponbob_squelpen@yahoo.com>
2013-06-27 19:47:08 +0700

    implement for minimalist mode

--- client/src/com/amca/android/replace/place/PlaceSelectorArrayAdapter.java
+++ client/src/com/amca/android/replace/place/PlaceSelectorArrayAdapter.java
@@ -3,12 +3,11 @@ package com.amca.android.replace.place;
 import java.text.DecimalFormat;
 import java.util.List;
 import com.amca.android.replace.R;
+import com.amca.android.replace.Setting;
 import com.amca.android.replace.model.Place;
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,33 +15,33 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PlaceSelectorArrayAdapter extends
-		ArrayAdapter<Place> {
+public class PlaceSelectorArrayAdapter extends ArrayAdapter<Place> {
+	private int view;
 	private final Context context;
 	private final List<Place> values;
 
-	public PlaceSelectorArrayAdapter(Context context,
-			List<Place> values) {
-		super(context, R.layout.activity_place_selector_row, values);
+	public PlaceSelectorArrayAdapter(Context context, List<Place> values,
+			int view) {
+		super(context, view, values);
+
 		this.context = context;
 		this.values = values;
+		this.view = view;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.activity_place_selector_row,
-				parent, false);
+		View rowView = inflater.inflate(view, parent, false);
 		TextView placeName = (TextView) rowView.findViewById(R.id.placeName);
-		TextView placeAddress = (TextView) rowView.findViewById(R.id.placeAddress);
+		TextView placeAddress = (TextView) rowView
+				.findViewById(R.id.placeAddress);
 		TextView placeDesc = (TextView) rowView.findViewById(R.id.placeDesc);
-		TextView placePoint = (TextView) rowView
-				.findViewById(R.id.placePoint);
+		TextView placePoint = (TextView) rowView.findViewById(R.id.placePoint);
 		TextView placeDistance = (TextView) rowView
 				.findViewById(R.id.placeDistance);
-		ImageView placeIcon = (ImageView) rowView.findViewById(R.id.placeIcon);
-		
+
 		DecimalFormat df = new DecimalFormat("#0.##");
 
 		Place place = values.get(position);
@@ -50,12 +49,19 @@ public class PlaceSelectorArrayAdapter extends
 		placeAddress.setText(place.getPlaceAddress());
 		placeDesc.setText(place.getPlaceDesc());
 		placePoint.setText(df.format(place.getAveragePoint()) + "/10");
-		placeDistance.setText(place.getPlaceReviews() + " review(s) | " + df.format(place.getPlaceDistance() / 1000) + " km");
-		
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-		String serverUrl = preferences.getString("serverUrl", "");
-		
-		UrlImageViewHelper.setUrlDrawable(placeIcon, serverUrl + "static/place/" + place.getPlaceId() + ".jpg", R.drawable.gif_loader);
+		placeDistance.setText(place.getPlaceReviews() + " review(s) | "
+				+ df.format(place.getPlaceDistance() / 1000) + " km");
+
+		Setting setting = new Setting();
+		if (!setting.getBoolean(getContext(), "minimalist")) {
+			String serverUrl = setting.getString(getContext(), "serverUrl");
+
+			ImageView placeIcon = (ImageView) rowView
+					.findViewById(R.id.placeIcon);
+			UrlImageViewHelper.setUrlDrawable(placeIcon, serverUrl
+					+ "static/place/" + place.getPlaceId() + ".jpg",
+					R.drawable.gif_loader);
+		}
 
 		return rowView;
 	}
